5eee6c72bcb0122f35b19c584a22d7a8b4d147b6
jalehoux <jalehoux@gmail.com>
2018-10-21 15:04:04 -0400

    added stuff

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql');
+var connection;
 
-const connection = 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection =
     mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'AZ8GwyF3?', // Add your password
-    database : 'burgers_db' // Add your database
-  });
+      host: 'localhost',
+      user: 'root',
+      password: 'AZ8GwyF3?', // Add your password
+      database: 'burgers_db' // Add your database
+    });
+}
 
-  module.exports = connection;
+module.exports = connection;
